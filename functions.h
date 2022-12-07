74ffe1871443e2e09b7dd3f9edc73c9d58df6713
Galen Schick <114014403+gasn26@users.noreply.github.com>
2022-12-06 19:48:53 -0600

    comment functions.h

--- functions.h
+++ functions.h
@@ -14,6 +14,7 @@ void move_to_rear(queue<T>& q) {
 
 template<class T>
 void printQ(queue<T> q) {
+    // Pass by value, then pop off elements from the front of the queue until empty
     while (q.size() > 0) {
         cout << q.front() << " ";
         q.pop();
@@ -23,14 +24,14 @@ void printQ(queue<T> q) {
 
 // QUESTION 2
 bool isPalindrome(string s, int lookAt = 0) {
-    if (lookAt > s.length() / 2 - 1) return true;
-    if (s.at(lookAt) != s.at(s.length() - 1 - lookAt)) return false;
-    return isPalindrome(s, lookAt+1);
+    if (lookAt > s.length() / 2 - 1) return true; // for even length values, stop at the length / 2 place. For odd, stop right before the center
+    if (s.at(lookAt) != s.at(s.length() - 1 - lookAt)) return false; // see if character matches opposing character in word
+    return isPalindrome(s, lookAt+1); // otherwise, look at the next characters
 }
 
 // QUESTION 5
 template<class T1, class T2>
 void printM(map<T1, T2> m) {
-    for (pair<T1, T2> p : m)
+    for (pair<T1, T2> p : m) // map is made of pairs
         cout << p.first << " : " << p.second << endl;
 }
